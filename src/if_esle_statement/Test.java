3f0766343acb2bd3b8bf279510beed77931a5b89
preetish1234 <preetish.m004@gmail.com>
2016-12-03 03:53:02 +0530

    if else

---
+++ src/if_esle_statement/Test.java
@@ -0,0 +1,30 @@
+package if_esle_statement;
+
+public class Test {
+
+
+	
+	
+	public static void main(String[] args) {
+		
+		String user = "preetish";	
+
+		String pass=   "1234";
+		
+    	if (user == "preetis"){
+    		if(pass == "1234")
+    		{
+    			
+    			System.out.println("user is valid");	
+    		}
+    	
+    	}
+    	else {
+    		System.out.println("Invalid username & password");
+    	}
+    	
+		
+
+	}
+
+}
